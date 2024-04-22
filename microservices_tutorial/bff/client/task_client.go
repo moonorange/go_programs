1689edcb9af21c007fe2f5b987a6e7a8909448aa
moonorange <monoma632@gmail.com>
2024-04-22 16:43:53 +0530

    fix: init client only once
    Signed-off-by: moonorange <monoma632@gmail.com>

--- microservices_tutorial/bff/client/task_client.go
+++ microservices_tutorial/bff/client/task_client.go
@@ -7,14 +7,21 @@ import (
 	"github.com/protogo/gen/genconnect"
 )
 
+var (
+	taskClient genconnect.TaskServiceClient
+)
+
 func NewTaskServiceClient(baseURL string) genconnect.TaskServiceClient {
 	// Set up a connection to the server.
 	// Create a gRPC client using the connect.WithGRPC() option
-	client := genconnect.NewTaskServiceClient(
+	if taskClient != nil {
+		return taskClient
+	}
+	taskClient = genconnect.NewTaskServiceClient(
 		http.DefaultClient,
 		baseURL,
 		connect.WithGRPC(),
 	)
 
-	return client
+	return taskClient
 }
